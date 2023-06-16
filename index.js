5222e3ca3898acad056f4991473ed74697baa210
Brian Kibet <kibetsbrian@gmail.com>
2023-06-16 13:44:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+function divide(a,b){
+
+    return 2000/100
+
+}
+console.log(divide())
+
+
+const square = x => x**2
+    
+console.log(square(2))
+
+const add = (a , b) => {
+    
+    return a + b
+}
+
+console.log(add(3,4))
